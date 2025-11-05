b18aa83bfbf3091de5a6c6afdd508334a846196f
yogeshwarreddy2002 <yogeshwarreddy416@gmail.com>
2025-11-05 20:44:55 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,22 @@
+from google import genai
+import streamlit as st
+st.title("🤖Smart Chat Bot")
+st.header("AI Chat Assistant")
+with st.sidebar:
+    st.title("⚙️ Settings")
+    st.write("This is your AI Chat Assistant.")
+    st.write("You can ask general knowledge, coding, help, ideas, etc.")
+    st.write("---")
+c1,c2=st.columns(2)
+with c1:
+  user_prompt=st.text_input("enter your prompt: ")
+with c2:
+  send_button=st.button("send")
+if user_prompt and send_button:
+ client = genai.Client(api_key="AIzaSyBdlcmcff1X7CUMgzar8P7rvV0XHQGf2YI")
+ response = client.models.generate_content(
+ model="gemini-2.5-flash", contents=user_prompt
+  )
+ st.title("AI Response:")
+ st.write(response.text)
+
